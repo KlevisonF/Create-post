d6648d801f7b6abfb6de68f33955224ab3248ed9
KlevisonF <silvaklevison11@gmail.com>
2025-01-10 00:27:43 -0300

    Adicionar função aos botao de publicar e apagar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment({content}){
+export function Comment({content, onDeleteComment}){
+
+function handleDeleteComment(){
+    onDeleteComment(content)
+}
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src='https://github.com/KlevisonF.png' />
@@ -14,7 +18,7 @@ export function Comment({content}){
                             <strong>Klevison Ferreira</strong>
                             <time title='11 de Maio ás 08:13h' dateTime='2022-05-11 08:13:39'>P  há 1h</time>
                         </div>
-                        <button  title='Deletar comentario'>
+                        <button onClick={handleDeleteComment}  title='Deletar comentario'>
                             <Trash size={24}/>
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,47 +1,59 @@
 import styles from './Post.module.css'
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
-import {format, formatDistanceToNow} from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
 import { useState } from 'react'
 import { use } from 'react'
 
 
-export function Post({author, publishedAt, content}) {
+export function Post({ author, publishedAt, content }) {
 
     const [comments, setComments] = useState([
         'Post muito bacana'
     ])
 
     const [newCommentText, SetNewCommentText] = useState('')
-    
+
     const publishedDateFormat = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
         locale: ptBr,
     })
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt,{
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
         locale: ptBr,
-       addSuffix: true
+        addSuffix: true
     })
-     function handleCreateNewComment(e){
+    function handleCreateNewComment(e) {
         e.preventDefault();
-        
-      
-      
+
+
+
         setComments([...comments, newCommentText]);
         SetNewCommentText('')
 
-       
-     }
 
-     function handleNewCommentChange(){
+    }
+
+    function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         SetNewCommentText(event.target.value)
-     }
+    }
+
+    function onDeleteComment(commentToDelete) {
+        const commentsWithoutDeleteone = comments.filter(comment =>{
+            return comment === commentToDelete;
+        })
+       setComments(commentsWithoutDeleteone)
+    }
+
+    function handleCreateNewCommentInvalid(){
+        event.target.setCustomValidity('Esse campo é obrigatorio')
+    }
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                      <Avatar  src={author.avatarUrl} />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
                         <strong>{author.name}</strong>
                         <span>{author.role}</span>
@@ -52,10 +64,10 @@ export function Post({author, publishedAt, content}) {
                 </time>
             </header>
             <div className={styles.content}>
-            {content.map(line =>{
-                    if(line.type === 'paragraph'){
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
                         return <p key={line.content}>{line.content}</p>
-                    }else if(line.type === 'link'){
+                    } else if (line.type === 'link') {
                         return <p key={line.content}><a href='#'>{line.content}</a></p>
                     }
                 })}
@@ -68,17 +80,24 @@ export function Post({author, publishedAt, content}) {
                     placeholder='Deixe um Comentario'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleCreateNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button 
+                    disabled={newCommentText.length === 0} 
+                    type='submit'>Publicar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
-              {comments.map(comment =>{
-                return <Comment key={comment} content={comment}/>
-                    
-               
-              })}
+                {comments.map(comment => {
+                    return <Comment
+                        key={comment}
+                        content={comment}
+                        onDeleteComment={onDeleteComment} />
+
+
+                })}
             </div>
 
         </article>
